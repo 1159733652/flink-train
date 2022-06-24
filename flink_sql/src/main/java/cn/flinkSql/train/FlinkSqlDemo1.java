a73ed10a5a92018d07fd86696a34b1b5ef1840f8
张洪瑀 <zhanghongyu1@bonc.com.cn>
2022-06-24 18:06:58 +0800

    add flink sql modul

---
+++ flink_sql/src/main/java/cn/flinkSql/train/FlinkSqlDemo1.java
@@ -0,0 +1,12 @@
+package cn.flinkSql.train;
+
+/**
+ * @Author zhanghongyu
+ * @Date 2022/6/24
+ * @DESC flink sql 学习
+ */
+public class FlinkSqlDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
