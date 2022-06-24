673f88e5a13d7fb89a88dddbbfd43751028672d2
张洪瑀 <zhanghongyu1@bonc.com.cn>
2022-06-24 17:17:52 +0800

    first demo

---
+++ flink_demo/src/main/java/cn/flink/demo/WordCount.java
@@ -0,0 +1,11 @@
+package cn.flink.demo;
+
+/**
+ * @Author zhanghongyu
+ * @Date 2022/6/24
+ * @DESC flink入门程序案例，
+ *          使用socket流式读取数据
+ *          nc -lk 9999  开启一个socket服务
+ */
+public class WordCount {
+}
